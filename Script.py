efcfac77d70195e72cdeebb3e1dc740f857ff680
Rebel <110738599+R4be1@users.noreply.github.com>
2023-03-16 15:49:02 +0800

    A few words from the smelly fart administrator
    These simple functions are mainly used to assist Python security developers, if you need more detailed changes or have questions, you can contact 2354961312@qq.com

--- Script.py
+++ Script.py
@@ -8,6 +8,4 @@ def RANDOM(num:int=32)->str:
 def LoadModules(PATH:"Folder's path")->list:
     #Get all the py module files in the folder.
     #Note that this function will get all the py files under the path, including subfolders.
-    ModuleList=[]
-    [ModuleList.append(os.path.join(root,file).replace(PATH,"").replace(".py","").replace("\\",".")) for root,dirs,files in os.walk(PATH) for file in files if file.endswith(".py")]
-    return ModuleList
+    return [os.path.join(root,file).replace(PATH,"").replace(".py","").replace("\\",".") for root,dirs,files in os.walk(PATH) for file in files if file.endswith(".py")]
