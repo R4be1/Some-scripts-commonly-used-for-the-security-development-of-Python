f384b961352e742268327559a772d23b461cc954
Rebel <110738599+R4be1@users.noreply.github.com>
2023-03-16 15:40:36 +0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,14 @@
-def RANDOM():
+def RANDOM(num:int=32)->str:
+    #Random choice 0-9a-zA-z.
+    #You can control the size of the return value through the num parameter.
     import random
-    _RANDOM="".join([random.choice("1234567890abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(32)])
+    _RANDOM="".join([random.choice("1234567890abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(num)])
     return _RANDOM
+
+def LoadModule(PATH:"Folder's path")->list:
+    #Get all the py module files in the folder.
+    #Note that this function will get all the py files under the path, including subfolders.
+    ModuleList=[]
+    for root,dirs,files in os.walk(PATH):
+        [ModuleList.append(os.path.join(root,file).replace(PATH,"").replace(".py","").replace("\\",".")) for file in files if file.endswith(".py")]
+    return ModuleList
